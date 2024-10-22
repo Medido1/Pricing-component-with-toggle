0d1a8d14a861c58ef30a47cba81fa5b94acdf93d
Medido1 <medi1222@gmail.com>
2024-10-22 22:06:57 +0100

    Set up desktop desing and add functionality to toggle button

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const toggleBtn = document.querySelector(".btn_toggle");
+const priceBasic = document.querySelector(".price.basic");
+const priceProfessional = document.querySelector(".price.professional");
+const priceMaster = document.querySelector(".price.master");
 
 toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.toggle("right");
+
+  if (toggleBtn.classList.contains("right")){
+    /* monthly prices */
+    toggleBtn.setAttribute("aria-label", "Switch to Annual Billing");
+    priceBasic.textContent = "19.99";
+    priceProfessional.textContent = "24.99";
+    priceMaster.textContent = "39.99";
+  } else {
+    /* Annual prices */
+    toggleBtn.setAttribute("aria-label", "Switch to Monthly Billing");
+    priceBasic.textContent = "199.99";
+    priceProfessional.textContent = "249.99";
+    priceMaster.textContent = "399.99";
+  }
 })
